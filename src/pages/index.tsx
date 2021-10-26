c8a3c97a4c7b26a290db78d5077dbc66b570966b
ooooorobo <orobos654@gmail.com>
2021-10-26 21:40:16 +0900

    feat: Move src files and add eslint&prettier&ts config

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '@src/styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+        hi!
+    </div>
+  )
+}
+
+export default Home
