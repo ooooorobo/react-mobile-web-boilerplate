1acbc33d2de3c139aeb49ad02041beb0f97be56f
ooooorobo <orobos654@gmail.com>
2021-10-30 16:52:04 +0900

    fix: adjust lint style

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,29 +1,29 @@
-import 'styled-components'
+import "styled-components";
 
-declare module 'styled-components' {
-    export interface DefaultTheme {
-        color: {
-            primary: string
-            secondary: string
-            point: string
-            black: string
-            gray5: string
-            gray4: string
-            gray3: string
-            gray2: string
-            gray1: string
-            gray0: string
-            white: string
-            success: string
-            danger: string
-            warning: string
-            info: string
-        }
-        window: {
-            pc: string
-            tab: string
-            mobile: string
-            mobileS: string
-        }
-    }
+declare module "styled-components" {
+  export interface DefaultTheme {
+    color: {
+      primary: string;
+      secondary: string;
+      point: string;
+      black: string;
+      gray5: string;
+      gray4: string;
+      gray3: string;
+      gray2: string;
+      gray1: string;
+      gray0: string;
+      white: string;
+      success: string;
+      danger: string;
+      warning: string;
+      info: string;
+    };
+    window: {
+      pc: string;
+      tab: string;
+      mobile: string;
+      mobileS: string;
+    };
+  }
 }
